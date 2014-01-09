a84e417e59bfb51f6c5a39534d3ebedeff20ead7
Karlo <Karlo@Aton>
2014-01-09 12:38:14 +0100

    First try

---
+++ src/me/i3ick/freezetag/PlayerSorter.java
@@ -0,0 +1,23 @@
+package me.i3ick.freezetag;
+
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class PlayerSorter implements Listener{
+	
+	private freezetagMain plugin;
+	
+	public PlayerSorter (freezetagMain plugin){
+		this.plugin = plugin;
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerChat(AsyncPlayerChatEvent e){
+		if(plugin.frozen.contains(e.getPlayer().getName())){
+			e.getPlayer().teleport(e.getPlayer().getLocation());
+		}
+	}
+}
